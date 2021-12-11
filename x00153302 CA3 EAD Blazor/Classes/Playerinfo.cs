f6a94051e8bdc24abc0610847bef4af4c0ef61ca
Muhammadkorumtallee <shuaibkorumtallee00@gmail.com>
2021-12-11 14:26:40 +0000

    search

---
+++ x00153302 CA3 EAD Blazor/Classes/Playerinfo.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace Basketball_CA3_EAD.Classes
+{
+    public class Team
+    {
+        public string city { get; set; }
+        public string division { get; set; }
+        public string full_name { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Player
+    {
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string position { get; set; }
+        public Team team { get; set; }
+        public List<Player> data { get; set; }
+    }
+}
